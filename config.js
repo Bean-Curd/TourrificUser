a553682973061b83a60da03f34b53679fc5bb0c9
Ashley Goh <116317152+Bean-Curd@users.noreply.github.com>
2024-01-10 08:28:18 +0800

    Add files via upload

---
+++ config.js
@@ -0,0 +1,10 @@
+export const FIREBASE_CONFIG = {
+  apiKey: "AIzaSyBJDkz5JXbMFsB03L_01NxJGGMJb-qyYKs",
+  authDomain: "dda-firebase-db6ca.firebaseapp.com",
+  databaseURL:
+    "https://dda-firebase-db6ca-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "dda-firebase-db6ca",
+  storageBucket: "dda-firebase-db6ca.appspot.com",
+  messagingSenderId: "1059621230047",
+  appId: "1:1059621230047:web:a889d6e2592b2081aa606a",
+};
